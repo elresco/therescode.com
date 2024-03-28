31a8a6f607f989588244341e94c093f1f044aa7d
lresco <luis.rebla@gmail.com>
2024-03-29 00:34:31 +0100

    removed export

--- next.config.js
+++ next.config.js
@@ -6,19 +6,10 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  output: 'export',
   reactStrictMode: true,
   wcMinify: true,
   images: { unoptimized: true },
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 
 export default config;
