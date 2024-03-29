74fe6c316c5a4a03f346e08bb935cc9b58f60212
lresco <luis.rebla@gmail.com>
2024-03-29 01:40:24 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ export default function Home() {
             <a className="px-4 py-2 rounded-lg bg-green-500 text-white font-bold border-2 border-green-500 hover:bg-white hover:text-green-500" href="https://btcpay.educabitcoin.com">BTC Pay Server</a>
           </div>
           <div className="flex gap-2">
-            <a className="px-4 py-2 rounded-lg bg-orange-500 text-white font-bold border-2 border-orange-500 hover:bg-white hover:text-orange-500" href="https://github.com/therescode">GitHub</a>
-            <a className="px-4 py-2 rounded-lg bg-orange-500 text-white font-bold border-2 border-orange-500 hover:bg-white hover:text-orange-500" href="https://x.com/therescode">X</a>
+            <a className="px-4 py-2 rounded-lg bg-indigo-500 text-white font-bold border-2 border-indigo-500 hover:bg-white hover:text-indigo-500" href="https://github.com/therescode">GitHub</a>
+            <a className="px-4 py-2 rounded-lg bg-gray-900 text-white font-bold border-2 border-gray-900 hover:bg-white hover:text-gray-900" href="https://x.com/therescode">X</a>
           </div>
         </div>
       </main>
