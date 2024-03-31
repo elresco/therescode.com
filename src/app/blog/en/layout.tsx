655f57591bef26884b16fcedd698ea1437efa2f0
lresco <luis.rebla@gmail.com>
2024-03-31 17:42:18 +0200

    fixed children errors

--- src/app/blog/en/layout.tsx
+++ src/app/blog/en/layout.tsx
@@ -2,5 +2,5 @@ import BlogLayout from "~/component/BlogLayout"
 import "~/styles/globals.css"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <BlogLayout children={children}></BlogLayout>
+  return <BlogLayout>{children}</BlogLayout>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function HomePage() {
           Rescode
         </h1>
         <div className="flex flex-row gap-2">
-          <Link target="_blank" className="flex-grow text-3xl p-4 rounded-lg bg-purple-500 text-white font-bold border-2 border-purple-500 hover:bg-white hover:text-purple-500" href={"https://github.com/therescode"}>GitHub</Link>
-          <Link target="_blank" className="flex-grow text-3xl p-4 rounded-lg bg-gray-900 text-white font-bold border-2 border-gray-900 hover:bg-white hover:text-gray-900" href={"https://x.com/therescode"}>x</Link>
-          <Link target="_blank" className="flex-grow text-3xl p-4 rounded-lg bg-orange-500 text-white font-bold border-2 border-orange-500 hover:bg-white hover:text-orange-500" href={"https://educabitcoin.com"}>EducaBitcoin</Link>
+          <Link className="flex-grow text-3xl p-4 rounded-lg bg-purple-500 text-white font-bold border-2 border-purple-500 hover:bg-white hover:text-purple-500" href={"https://github.com/therescode"}>GitHub</Link>
+          <Link className="flex-grow text-3xl p-4 rounded-lg bg-gray-900 text-white font-bold border-2 border-gray-900 hover:bg-white hover:text-gray-900" href={"https://x.com/therescode"}>x</Link>
+          <Link className="flex-grow text-3xl p-4 rounded-lg bg-orange-500 text-white font-bold border-2 border-orange-500 hover:bg-white hover:text-orange-500" href={"https://educabitcoin.com"}>EducaBitcoin</Link>
         </div>
       </div>
     </main>
